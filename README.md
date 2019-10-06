276f48d8e8c3ab66342fc8267d1e6511a29f3bee
LR-LR <elhaire.aboudou@gmail.com>
2019-10-06 17:10:49 +0200

    setting up firebase and change readme

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 This chat is designed using the [ReactJS](https://reactjs.org/) framework and a [Firebase](https://firebase.google.com/) database (thanks to [Facebook](https://facebook.com/) and [Google](https://google.com/)!).
 
 A version with a back-end designed by me using [NodeJS](https://nodejs.org/) and a [MongoDB](https://www.mongodb.com) database will be available after it is completed.
+
+## Try
+
+Pour essayer le chat dans votre ordinateur, téléchargez ou clonez ce repo et exécuter la commande `npm install` avant de faire `npm start`.
--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const firebase = require('firebase');
+require('firebase/firestore');
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyBWnJHpuplBs8wVe88ypNB1GPI3wAPikwg',
+  authDomain: 'lr-chat-react.firebaseapp.com',
+  databaseURL: 'https://lr-chat-react.firebaseio.com',
+  projectId: 'lr-chat-react',
+  storageBucket: 'lr-chat-react.appspot.com',
+  messagingSenderId: '799868362297',
+  appId: '1:799868362297:web:2fb8dfe62aa827d65137e8',
+  measurementId: 'G-V766V8TQBS'
+});
+
+ReactDOM.render(<div>Hello world !</div>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
