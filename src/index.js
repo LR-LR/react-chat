3d5fb7b4603aee5215b820ff4ae9fc223edc3e3a
LR-LR <elhaire.aboudou@gmail.com>
2019-10-12 16:17:34 +0200

    again modify index.js for tet build netlify

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-// import Dashboard from './Dashboard/dashboard';
-import './index.css';
-import Login from './Login/login';
-import * as serviceWorker from './serviceWorker';
-import Signup from './Signup/signup';
+import Dashboard from '../src/Dashboard/dashboard';
+import '../src/index.css';
+import Login from '../src/Login/login';
+import * as serviceWorker from '../src/serviceWorker';
+import Signup from '../src/Signup/signup';
 
 const firebase = require('firebase');
 require('firebase/firestore');
