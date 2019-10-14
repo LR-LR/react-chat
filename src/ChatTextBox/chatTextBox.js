c104b5c557e97803ac1e0b8f20c4c76d4c6748c7
LR-LR <elhaire.aboudou@gmail.com>
2019-10-14 18:56:46 +0200

    end of the app

---
+++ src/ChatTextBox/chatTextBox.js
@@ -0,0 +1,50 @@
+import TextField from '@material-ui/core/TextField';
+import Send from '@material-ui/icons/Send';
+import styles from './styles';
+import { withStyles } from '@material-ui/core/styles';
+
+import React, { Component } from 'react';
+
+class ChatTextBox extends Component {
+  constructor() {
+    super();
+    this.state = {
+      chatText: ''
+    };
+  }
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className={classes.chatTextBoxContainer}>
+        <TextField
+          placeholder="Type your message..."
+          onKeyUp={(e) => this.userTyping(e)}
+          id="chattextbox"
+          className={classes.chatTextBox}
+          onFocus={this.userClickedInput}></TextField>
+        <Send onClick={this.submitMessage} className={classes.sendBtn}></Send>
+      </div>
+    );
+  }
+
+  userClickedInput = () => {
+    this.props.messageReadFn();
+  };
+
+  userTyping = (e) =>
+    e.keyCode === 13
+      ? this.submitMessage()
+      : this.setState({ chatText: e.target.value });
+
+  messageValid = (txt) => txt && txt.replace(/\s/g, '').length;
+
+  submitMessage = () => {
+    if (this.messageValid(this.state.chatText)) {
+      this.props.submitMessageFn(this.state.chatText);
+      document.getElementById('chattextbox').value = '';
+    }
+  };
+}
+
+export default withStyles(styles)(ChatTextBox);
--- src/dashboard/dashboard.js
+++ src/dashboard/dashboard.js
@@ -1,7 +1,174 @@
 import React, { Component } from 'react';
+import ChatList from '../ChatList/chatList';
+import { Button, withStyles } from '@material-ui/core';
+import styles from './styles';
+import ChatView from '../ChatView/chatView';
+import ChatTextBox from '../ChatTextBox/chatTextBox';
+import NewChat from '../NewChat/newChat';
+const firebase = require('firebase');
 
-export default class Dashboard extends Component {
-  render() {
-    return <div>Hello World from Dashboard !</div>;
+class Dashboard extends Component {
+  constructor() {
+    super();
+    this.state = {
+      selectedChat: null,
+      newChatFormVisible: false,
+      email: null,
+      chats: []
+    };
   }
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <div>
+        <ChatList
+          history={this.props.history}
+          newChatBtnFn={this.newChatBtnClicked}
+          selectChatFn={this.selectChat}
+          chats={this.state.chats}
+          userEmail={this.state.email}
+          selectedChatIndex={this.state.selectedChat}></ChatList>
+        {this.state.newChatFormVisible ? null : (
+          <ChatView
+            user={this.state.email}
+            chat={this.state.chats[this.state.selectedChat]}></ChatView>
+        )}
+        {this.state.selectedChat !== null && !this.state.newChatFormVisible ? (
+          <ChatTextBox
+            submitMessageFn={this.submitMessage}
+            messageReadFn={this.messageRead}></ChatTextBox>
+        ) : null}
+        {this.state.newChatFormVisible ? (
+          <NewChat
+            goToChatFn={this.goToChat}
+            newChatSubmitFn={this.newChatSubmit}></NewChat>
+        ) : null}
+        <Button className={classes.signOutBtn} onClick={this.signOut}>
+          Sign Out
+        </Button>
+      </div>
+    );
+  }
+
+  signOut = () => {
+    firebase.auth().signOut();
+  };
+
+  selectChat = async (chatIndex) => {
+    await this.setState({
+      selectedChat: chatIndex,
+      newChatFormVisible: false
+    });
+    this.messageRead();
+  };
+
+  submitMessage = (msg) => {
+    const docKey = this.buildDocKey(
+      this.state.chats[this.state.selectedChat].users.filter(
+        (_usr) => _usr !== this.state.email
+      )[0]
+    );
+    firebase
+      .firestore()
+      .collection('chats')
+      .doc(docKey)
+      .update({
+        messages: firebase.firestore.FieldValue.arrayUnion({
+          sender: this.state.email,
+          message: msg,
+          timestamp: Date.now()
+        }),
+        receiverHasRead: false
+      });
+  };
+
+  buildDocKey = (friend) => [this.state.email, friend].sort().join(':');
+
+  newChatBtnClicked = () => {
+    this.setState({
+      newChatFormVisible: true,
+      selectedChat: null
+    });
+  };
+
+  messageRead = () => {
+    const docKey = this.buildDocKey(
+      this.state.chats[this.state.selectedChat].users.filter(
+        (_usr) => _usr !== this.state.email
+      )[0]
+    );
+    if (this.clickedChatWhereNotSender(this.state.selectedChat)) {
+      firebase
+        .firestore()
+        .collection('chats')
+        .doc(docKey)
+        .update({
+          receiverHasRead: true
+        });
+    } else {
+      console.log('Clicked msg where the user whas the sender');
+    }
+  };
+
+  goToChat = async (docKey, msg) => {
+    const usersInChat = docKey.split(':');
+    const chat = this.state.chats.find((_chat) =>
+      usersInChat.every((_user) => _chat.users.includes(_user))
+    );
+    this.setState({
+      newChatFormVisible: false
+    });
+    await this.selectChat(this.state.chats.indexOf(chat));
+    this.submitMessage(msg);
+  };
+
+  newChatSubmit = async (chatObj) => {
+    const docKey = this.buildDocKey(chatObj.sendTo);
+    await firebase
+      .firestore()
+      .collection('chats')
+      .doc(docKey)
+      .set({
+        receiverHasRead: false,
+        users: [this.state.email, chatObj.sendTo],
+        messages: [
+          {
+            message: chatObj.message,
+            sender: this.state.email
+          }
+        ]
+      });
+    this.setState({
+      newChatFormVisible: false
+    });
+    this.selectChat(this.state.chats.length - 1);
+  };
+
+  clickedChatWhereNotSender = (chatIndex) =>
+    this.state.chats[chatIndex].messages[
+      this.state.chats[chatIndex].messages.length - 1
+    ].sender !== this.state.email;
+
+  componentDidMount = () => {
+    firebase.auth().onAuthStateChanged(async (_usr) => {
+      if (!_usr) {
+        this.props.history.push('/login');
+      } else {
+        await firebase
+          .firestore()
+          .collection('chats')
+          .where('users', 'array-contains', _usr.email)
+          .onSnapshot(async (res) => {
+            const chats = res.docs.map((_doc) => _doc.data());
+            await this.setState({
+              email: _usr.email,
+              chats: chats
+            });
+          });
+      }
+    });
+  };
 }
+
+export default withStyles(styles)(Dashboard);
